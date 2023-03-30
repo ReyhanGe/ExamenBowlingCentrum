0dc3a477ccb37bc3d5a8c00d7335b600fa5db580
Reyhan Gedik <334480@student.mboutrecht.nl>
2023-03-30 09:30:28 +0200

    config sturen

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'examenbowlingcenter');
+
+define('APPROOT', dirname(dirname(__FILE__))) ;
+
+define('URLROOT', 'http://examenbowlingcenter.nl/');
+
+define('SITENAME', 'MVC Framework reference');
+
+?>
